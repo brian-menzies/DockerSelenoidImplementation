bc0fb1928dd24f7974eefda1e15ab999d39df136
brian-menzies <brian.menzies@orasi.com>
2017-11-21 16:12:21 -0500

    Made third Test Files

--- Tests/GoogleAutomationIE.py
+++ Tests/GoogleAutomationIE.py
@@ -2,12 +2,12 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# get the path of ChromeDriverServer
+# get the path of IEDriverServer
 dir = os.path.dirname(__file__)
-chrome_driver_path = dir + "\chromedriver.exe"
+ie_driver_path = dir + "\IEDriverServer.exe"
 
-# create a new Chrome session
-driver = webdriver.Chrome(chrome_driver_path)
+# create a new Internet Explorer session
+driver = webdriver.Ie(ie_driver_path)
 driver.implicitly_wait(30)
 driver.maximize_window()
 
