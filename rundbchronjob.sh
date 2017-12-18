b1cf5480a8057be5ca1eb86155a3a5d0d18ed9fb
brian-menzies <brian.menzies@orasi.com>
2017-12-18 10:09:57 -0500

    Update rundbchronjob.sh

--- rundbchronjob.sh
+++ rundbchronjob.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-kill -9 1337
-python36 createdbfile.py
-cd quotafile
-python -m SimpleHTTPServer 1337 &
-cd ..
+while true
+do
+ kill -9 1337
+ sleep 30
+ python36 createdbfile.py
+ cd quotafile
+ python -m SimpleHTTPServer 1337 &
+ cd ..
+done
