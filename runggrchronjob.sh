317b255ec5b07bedf3df4300e9a7c16f324bec6a
brian-menzies <brian.menzies@orasi.com>
2018-01-03 11:13:50 -0500

    Update runggrchronjob.sh

--- runggrchronjob.sh
+++ runggrchronjob.sh
@@ -2,6 +2,6 @@
 while true
 do
  sleep 10
- curl --silent 'http://54.145.92.108:1337/orasitester.xml' > /etc/grid-router/quota/orasitester.xml
+ curl --silent '' > /etc/grid-router/quota/orasitester.xml
  docker kill -s HUP ggrbalancer
 done
